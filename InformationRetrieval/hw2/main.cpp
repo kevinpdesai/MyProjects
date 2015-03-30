17ed5170d442a662baee443ac8135c7b9112eb7e
kevinpdesai <kevinpdesai@gmail.com>
2015-03-29 21:15:22 -0500

    Added Gamma, front and block coding.

--- InformationRetrieval/hw2/main.cpp
+++ InformationRetrieval/hw2/main.cpp
@@ -32,8 +32,8 @@ ofstream version1_uncomp("Index_Version1.uncompress");
 ofstream version2_uncomp("Index_Version2.uncompress");
 
 // Compressed Version index files.
-//ofstream version1_comp("Index_Version1.compress");
-//ofstream version2_comp("Index_Version2.compress");
+ofstream version1_comp("Index_Version1.compress");
+ofstream version2_comp("Index_Version2.compress");
 
 // Variable used to calculate the total number of lemmas/stems (Words).
 long nFileWords = 0;
@@ -70,6 +70,13 @@ map<string, vector<pair<int, int> > > lemmas;
 // Map to store the STEMS
 map<string, int> stemsDict;
 map<string, vector<pair<int, int> > > stems;
+
+// Map to store pointers (addresses) for the index words.
+map<string, int> blockCodingWordPointer;
+map<string, int> frontCodingWordPointer;
+map<string, unsigned char> frontCodingWordCountPointer;
+map<string, int> lemmaUncompPointer;
+map<string, int> stemUncompPointer;
 
 // Vector to keep the sorted dictionary of lemmas and stems.
 vector<pair<string,vector<pair<int, int> > > > sortedLemmasFreq;
@@ -167,6 +174,8 @@ void addLemma(string word, int doc_id) {
 	// Lemmatize the word.
 	char *c = lemmatize->Lemmatize(word.c_str());
 	string lemma(c);
+	if(lemma.length()<1)
+		return;
 	// Check if it is part of stopwords.
 	if(stopWords.count(lemma))
 		return;
@@ -190,9 +199,9 @@ void addStem(string word, int doc_id) {
 	char* c = (char*)word.c_str();
 	// Stem the word.
 	char* st = porter_stem(c);
-	if(!st)
-		return;
 	string stem(st);
+	if(stem.length()<1)
+		return;
 	// Check if it is part of stopwords.
 	if(stopWords.count(stem))
 		return;
@@ -462,22 +471,360 @@ void readStopWords(char file[]) {
 	return;
 }
 
-// Write doc info.
-void writeDocInfo(ofstream& f, map<int, pair<int,int> > m) {
-	for(int i=1; i<=m.size(); i++)
-		f << i << " " << m[i].first << " " << m[i].second << endl;
+// Block code the dictionary.
+void blockCode(ofstream& f, vector<pair<string,vector<pair<int, int> > > > m) {
+	// Take each word.
+	for(int i=0; i<m.size(); i++) {
+		// Get the current file pointer location and store it so that the word can be pointed to later while storing posting.
+		int l = f.tellp();
+		blockCodingWordPointer[m[i].first] = l;
+		// Write the length of the word as unsigned char and the word itself in the file. No delimiter between the words. The length of the word can uniquely identify the word end.
+		unsigned char c = m[i].first.length();
+		f << c << m[i].first;
+	}
+	// At the end enter the \n character for convenience and readability.
+	f << endl;
+}
+
+// Front code the dictionary.
+void frontCode(ofstream& f, vector<pair<string,vector<pair<int, int> > > > m) {
+	string s(""), ss("");
+	int fp=0;
+	set <string> lcps;
+	for (int i=0; i<m.size(); i++) {
+		ss = m[i].first;
+		// Find out if the word currently as root and current word have at least the first 4 letters common. If so insert it into a set of strings to be front coded together.
+		if (s.size() > 4 && ss.size() > 4 && s[0] == ss[0] && s[1] == ss[1] && s[2] == ss[2] && s[3] == ss[3])
+				lcps.insert(ss);
+		else {
+			s = ss;
+			// Skip string construction and compression if there are no words.
+			if (lcps.size() == 0) {
+				lcps.insert(s);
+				continue;
+			}
+			string first = *(lcps.begin());
+			char coded[2500];
+			unsigned int cid = 1, ci = 0;
+			bool incflag = true;
+
+			// Get the maximum common string length in cid
+			while (incflag) {
+				cid++;
+				for (set<string>::iterator iter = lcps.begin(); iter != lcps.end(); iter++) {
+					if((*iter).length() <= cid) {
+						incflag = false;
+						break;
+					}
+					if((*iter)[cid] != first[cid])
+						incflag = false;
+				}
+			}
+
+			// Write to file the standard string starting with its length as unsigned char.
+			coded[ci++] = (unsigned char)first.size();
+
+			while (ci <= cid) {
+				coded[ci] = first[ci-1];
+				ci++;
+			}
+
+			// Store the pointer address to the first word or the root word in the common words.
+			// Also, store the unsigned char '0' to identify that there is only one string and no common characters.
+			frontCodingWordPointer[first] = fp;
+			frontCodingWordCountPointer[first] = (unsigned char)0;
+			// Append words atop first word if there are any
+			if (lcps.size() != 1) {
+				int x = 0;
+				// Generate the compressed strings
+				for (set<string>::iterator iter = lcps.begin(); iter != lcps.end(); iter++) {
+					// Write '*' as the first character after the common letters.
+					if (*iter == first)
+						coded[ci++] = '*';
+					// Write the length of the uncommon part of the word remaning to be written and also store another character '#' to distinguish that uncommon part start.
+					else {
+						coded[ci++] = (unsigned char)((*iter).size() - cid);
+						coded[ci++] = '#';
+					}
+					// Store the pointer address of the words same as that of the root - common part pointer address.
+					// Also, store the number 'x' in increasing order so as to identify that this word is will occur after 'x' occurences of the '#' or '* symbols.
+					// This will help uniquely identify the words.
+					x++;
+					frontCodingWordPointer[*iter] = fp;
+					frontCodingWordCountPointer[*iter] = (unsigned char)x;
+					for (unsigned int i = cid; i < (*iter).size(); i++)
+						coded[ci++] = (*iter)[i];
+				}
+			}
+
+			coded[ci++] = '\0';
+			f << coded;
+			fp = f.tellp();
+			lcps.clear();
+			lcps.insert(s);
+		}
+	}
+	// Check and do the same if the iteration through the dictionary has ended and there are still words present in the common set.
+	if(lcps.size()!=0) {
+		string first = *(lcps.begin());
+		char coded[2500];
+		unsigned int cid = 1, ci = 0;
+		bool incflag = true;
+
+		// Get the maximum common string length in cid
+		while (incflag) {
+			cid++;
+			for (set<string>::iterator iter = lcps.begin(); iter != lcps.end(); iter++) {
+				if((*iter).length() <= cid) {
+					incflag = false;
+					break;
+				}
+				if((*iter)[cid] != first[cid])
+					incflag = false;
+			}
+		}
+
+		// Write to file the standard string starting with its length as unsigned char.
+		coded[ci++] = (unsigned char)first.size();
+
+		while (ci <= cid) {
+			coded[ci] = first[ci-1];
+			ci++;
+		}
+
+		// Store the pointer address to the first word or the root word in the common words.
+		// Also, store the unsigned char '0' to identify that there is only one string and no common characters.
+		frontCodingWordPointer[first] = fp;
+		frontCodingWordCountPointer[first] = (unsigned char)0;
+		// Append words atop first word if there are any
+		if (lcps.size() != 1) {
+			int x = 0;
+			// Generate the compressed strings
+			for (set<string>::iterator iter = lcps.begin(); iter != lcps.end(); iter++) {
+				// Write '*' as the first character after the common letters.
+				if (*iter == first)
+					coded[ci++] = '*';
+				// Write the length of the uncommon part of the word remaning to be written and also store another character '#' to distinguish that uncommon part start.
+				else {
+					coded[ci++] = (unsigned char)((*iter).size() - cid);
+					coded[ci++] = '#';
+				}
+				// Store the pointer address of the words same as that of the root - common part pointer address.
+				// Also, store the number 'x' in increasing order so as to identify that this word is will occur after 'x' occurences of the '#' or '* symbols.
+				// This will help uniquely identify the words.
+				x++;
+				frontCodingWordPointer[*iter] = fp;
+				frontCodingWordCountPointer[*iter] = (unsigned char)x;
+				for (unsigned int i = cid; i < (*iter).size(); i++)
+					coded[ci++] = (*iter)[i];
+			}
+		}
+
+		coded[ci++] = '\0';
+		f << coded;
+		fp = f.tellp();
+		lcps.clear();
+	}
+}
+
+// Get the binary string from the gamma/delta code.
+string binary(long x)
+{
+	string s;
+	do
+	{
+		s.push_back('0' + (x & 1));
+	} while (x >>= 1);
+	std::reverse(s.begin(), s.end());
+	return s;
+}
+
+// Get gamma code of the gap.
+string getGammaCode(unsigned int x) {
+	unsigned int t = x, c = 0, u = 0, b = 1, bit;
+	long ret = 0;
+	while (t) {
+		bit = (t & 1) ? 1 : 0;
+		b = b << 1;
+		b = b | bit;
+		t = t >> 1;
+		c++;
+	}
+	b = b >> 1;
+	c = 32 - c + 1;
+	u = ~u;
+	while (c--)
+		u = u >> 1;
+	u = u << 1;
+	ret = u;
+	while (b != 1) {
+		ret = ret << 1;
+		ret = (ret | ((b & 1) ? 1 : 0));
+		b = b >> 1;
+	}
+	return binary(ret);
+}
+
+// Get delta code of the gap.
+string getDeltaCode(unsigned int x) {
+	unsigned int t = x, c = 0, u = 0, b = 1, bit;
+	long ret = 0;
+	while (t) {
+		bit = (t & 1) ? 1 : 0;
+		b = b << 1;
+		b = b | bit;
+		t = t >> 1;
+		c++;
+	}
+	b = b >> 1;
+	c = 32 - c + 1;
+	u = ~u;
+	while (c--)
+		u = u >> 1;
+	u = u << 1;
+	ret = u;
+	while (b != 1) {
+		ret = ret << 1;
+		ret = (ret | ((b & 1) ? 1 : 0));
+		b = b >> 1;
+	}
+	return binary(ret);
+}
+
+// Gamma code the dictionary.
+void gammaCode(ofstream& f, vector<pair<string,vector<pair<int, int> > > > m) {
+	// Select each word in the dictionary.
+	for(int i=0; i<m.size(); i++) {
+		// Write the file address pointer to every 8th word for k=8 block coding.
+		if(i%8==0)
+			f << blockCodingWordPointer[m[i].first] << " ";
+		// Wrtie the size of the posting list.
+		f << m[i].second.size() << " ";
+		// Write the frequency of the word in each document.
+		for(int j=0; j<m[i].second.size(); j++)
+			f << m[i].second.at(j).second << " ";
+		// Get the first posting and get the gamma code for it.
+		int cg = m[i].second.at(0).first;
+		string gc = getGammaCode(cg);
+		string a;
+		int n=0;
+		// Write the code to the file byte by byte.
+		while(gc.length()>=8) {
+			a = gc.substr(0,8);
+			n = 128*((a[0]=='1')?1:0) + 64*((a[1]=='1')?1:0) + 32*((a[2]=='1')?1:0) + 16*((a[3]=='1')?1:0) + 8*((a[4]=='1')?1:0) + 4*((a[5]=='1')?1:0) + 2*((a[6]=='1')?1:0) + ((a[7]=='1')?1:0);
+			f << (unsigned char)n;
+			gc = gc.substr(8);
+		}
+		for(int j=1; j<m[i].second.size(); j++) {
+			// Get the gap between the postings and get the gamma code for them.
+			cg = m[i].second.at(j).first - m[i].second.at(j-1).first;
+			gc = gc + getGammaCode(cg);
+			// Write the code to the file byte by byte.
+			while(gc.length()>=8) {
+				a = gc.substr(0,8);
+				n = 128*((a[0]=='1')?1:0) + 64*((a[1]=='1')?1:0) + 32*((a[2]=='1')?1:0) + 16*((a[3]=='1')?1:0) + 8*((a[4]=='1')?1:0) + 4*((a[5]=='1')?1:0) + 2*((a[6]=='1')?1:0) + ((a[7]=='1')?1:0);
+				f << (unsigned char)n;
+				gc = gc.substr(8);
+			}
+		}
+		// Check if there is still code remaining. Append '0' at the end and write the bytes. The number of postings and the gamma code will determine the end of the file uniquely.
+		if(gc!="") {
+			n=0;
+			for(int k=0; k<gc.length(); k++)
+				n+=(128/((int)pow(2,k)))*((gc[k]=='1')?1:0);
+			f << (unsigned char)n;
+		}
+	}
+}
+
+// Delta code the dictionary.
+void deltaCode(ofstream& f, vector<pair<string,vector<pair<int, int> > > > m) {
+	// Select each word in the dictionary.
+	for(int i=0; i<m.size(); i++) {
+		// Write the file address pointer to every word for front coding.
+		f << frontCodingWordPointer[m[i].first] << " " << frontCodingWordCountPointer[m[i].first] << " ";
+		// Wrtie the size of the posting list.
+		f << m[i].second.size() << " ";
+		// Write the frequency of the word in each document.
+		for(int j=0; j<m[i].second.size(); j++)
+			f << m[i].second.at(j).second << " ";
+		// Get the first posting and get the delta code for it.
+		int cg = m[i].second.at(0).first;
+		string dc = getDeltaCode(cg);
+		string a;
+		int n=0;
+		// Write the code to the file byte by byte.
+		while(dc.length()>=8) {
+			a = dc.substr(0,8);
+			n = 128*((a[0]=='1')?1:0) + 64*((a[1]=='1')?1:0) + 32*((a[2]=='1')?1:0) + 16*((a[3]=='1')?1:0) + 8*((a[4]=='1')?1:0) + 4*((a[5]=='1')?1:0) + 2*((a[6]=='1')?1:0) + ((a[7]=='1')?1:0);
+			f << (unsigned char)n;
+			dc = dc.substr(8);
+		}
+		for(int j=1; j<m[i].second.size(); j++) {
+			// Get the gap between the postings and get the delta code for them.
+			cg = m[i].second.at(j).first - m[i].second.at(j-1).first;
+			dc = dc + getGammaCode(cg);
+			// Write the code to the file byte by byte.
+			while(dc.length()>=8) {
+				a = dc.substr(0,8);
+				n = 128*((a[0]=='1')?1:0) + 64*((a[1]=='1')?1:0) + 32*((a[2]=='1')?1:0) + 16*((a[3]=='1')?1:0) + 8*((a[4]=='1')?1:0) + 4*((a[5]=='1')?1:0) + 2*((a[6]=='1')?1:0) + ((a[7]=='1')?1:0);
+				f << (unsigned char)n;
+				dc = dc.substr(8);
+			}
+		}
+		// Check if there is still code remaining. Append '0' at the end and write the bytes. The number of postings and the delta code will determine the end of the file uniquely.
+		if(dc!="") {
+			n=0;
+			for(int k=0; k<dc.length(); k++)
+				n+=(128/((int)pow(2,k)))*((dc[k]=='1')?1:0);
+			f << (unsigned char)n;
+		}
+	}
 }
 
 // Write index.
-void writeIndex(ofstream& f, vector<pair<string,vector<pair<int, int> > > > m, bool comp = false) {
+void writeIndex(ofstream& f, vector<pair<string,vector<pair<int, int> > > > m, int version, bool comp = false) {
+	// Write the compressed version.
 	if(comp) {
-		;
+		// Version-1 Block coding & Gamma coding.
+		if(version==1) {
+			blockCode(f,m);
+			gammaCode(f,m);
+		}
+		// Version-2 Front coding & Delta coding.
+		else {
+			frontCode(f,m);
+			deltaCode(f,m);
+		}
 	}
+	// Write the uncompressed version.
 	else {
 		for(int i=0; i<m.size(); i++) {
-			f << m[i].first << " " << m[i].second.size();
+			// Get the current file pointer location and store it so that the word can be pointed to later while storing posting.
+			int l = f.tellp();
+			if(version==1)
+				lemmaUncompPointer[m[i].first] = l;
+			else
+				stemUncompPointer[m[i].first] = l;
+			// Write the length of the word as unsigned char and the word itself in the file. No delimiter between the words. The length of the word can uniquely identify the word end.
+			int c = m[i].first.length();
+			f << c << " " << m[i].first;
+			f << endl;
+		}
+		for(int i=0; i<m.size(); i++) {
+			// Write the file address pointer to every word.
+			if(version==1)
+				f << lemmaUncompPointer[m[i].first];
+			else
+				f << stemUncompPointer[m[i].first];
+			// Wrtie the size of the posting list.
+			f << " " << m[i].second.size();
+			// Write the frequency of the word in each document.
 			for(int j=0; j<m[i].second.size(); j++)
-				f << " " << m[i].second.at(j).first << " " << m[i].second.at(j).second;
+				f << " " << m[i].second.at(j).second;
+			// Write the posting document numbers.
+			for(int j=0; j<m[i].second.size(); j++)
+				f << " " << m[i].second.at(j).first;
 			f << endl;
 		}
 	}
@@ -516,15 +863,18 @@ int main(int argc, char *argv[]) {
 	// Print the results.
 	printResults();
 
-	writeDocInfo(version1_uncomp,docInfoLemmas);
-	writeDocInfo(version2_uncomp,docInfoStems);
-	writeIndex(version1_uncomp, sortedLemmasAlpha);
-	writeIndex(version2_uncomp,sortedStemsAlpha);
+	// Write the index files - compressed and uncompressed.
+	writeIndex(version1_uncomp, sortedLemmasAlpha, 1);
+	writeIndex(version2_uncomp, sortedStemsAlpha, 2);
+	writeIndex(version1_comp, sortedLemmasAlpha, 1, true);
+	writeIndex(version2_comp, sortedStemsAlpha, 2, true);
 
 
 	// Close files.
 	version1_uncomp.close();
 	version2_uncomp.close();
+	version1_comp.close();
+	version2_comp.close();
 
 	// End timer and calculate time.
 #ifdef linux
